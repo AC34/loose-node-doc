3102bc5a54ccdce8562643ad2753b37715d0f27e
ac1deb <grepoac34devahead@yahoo.co.jp>
2020-03-21 15:08:36 +0900

    adding comments

--- src/outputs/filter/otreeToRelativePaths.js
+++ src/outputs/filter/otreeToRelativePaths.js
@@ -1,8 +1,8 @@
-
 /**
  * turns all the absolute paths to relative paths from project root.
  * all the paths are supposed to be under the project root directory.
  * @param {otree} otree
+ * @@aram {string} project_root_dir
  * @preturn {otree} otree
  */
 function otreeToRelativePaths(otree,project_root_dir){
@@ -13,7 +13,11 @@ function otreeToRelativePaths(otree,project_root_dir){
   }
   return otree;
 }
-
+/**
+ * 
+ * @param {string} path 
+ * @param {string} project_root_dir 
+ */
 function toReleative(path,project_root_dir){
   if(path.startsWith(project_root_dir)){
     var ret = path.replace(project_root_dir,"");

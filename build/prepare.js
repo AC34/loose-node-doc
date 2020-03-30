abaf2a9235ef1ca7a07886bfd8c814617b2d909d
ac1win <grepoac34devahead@yahoo.co.jp>
2020-03-30 14:29:58 +0900

    adding template lister

--- build/prepare.js
+++ build/prepare.js
@@ -7,5 +7,8 @@ var langs_lister = require("./../src/outputs/lang/langs_lister");
 //update options keys list
 var options_lister = require("../src/Options/options_lister");
 
+//update templates list
+var template_lister = require("../src/arrange/html_page/templates_list/templates_lister");
+
 //push everything waiting for update
 
